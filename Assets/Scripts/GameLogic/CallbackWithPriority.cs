4a365d961dcab116fe93d9abe0555fe673388099
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-11-02 17:42:57 +0300

    +EventBus

---
+++ Assets/Scripts/GameLogic/CallbackWithPriority.cs
@@ -0,0 +1,10 @@
+﻿internal class CallbackWithPriority
+{
+    public readonly int Priority;
+    public readonly object Callback;
+    public CallbackWithPriority(int priority,object callback)
+    {
+        Priority = priority;
+        Callback = callback;
+    }
+}

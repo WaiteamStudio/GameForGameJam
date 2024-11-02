ed05dd6f3a3f8fc157cae2a9785c0e02d1429ac7
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-11-02 18:35:35 +0300

    event bus

--- Assets/Scripts/GameLogic/DamageDealer.cs
+++ Assets/Scripts/GameLogic/DamageDealer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Reflection;
+using UnityEngine;
 
 public class DamageDealer : MonoBehaviour
 {
@@ -12,5 +13,18 @@ public class DamageDealer : MonoBehaviour
             health.TakeDamage(damage);
             Debug.Log("Герой получил урон: " + damage);
         }
+    }
+    protected virtual void DealDamage(HealthComponent healthComponent, int damage)
+    {
+        healthComponent.TakeDamage(damage);
+    }
+}
+public class ElementalDamageDealer : DamageDealer
+{ 
+    [SerializeField]
+    PlayerForm  playerForm;
+    protected override void DealDamage(HealthComponent healthComponent, int damage)
+    {
+        healthComponent.TakeDamage((int)damage, playerForm);
     }
 }

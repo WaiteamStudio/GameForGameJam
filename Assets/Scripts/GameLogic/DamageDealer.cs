ec79393b3f3264fd5e2f90d1d458d5040ed53761
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-11-02 18:35:35 +0300

    event bus

---
+++ Assets/Scripts/GameLogic/DamageDealer.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 10;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HealthComponent health = collision.GetComponent<HealthComponent>();
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+            Debug.Log("Герой получил урон: " + damage);
+        }
+    }
+    protected virtual void DealDamage(HealthComponent healthComponent, int damage)
+    {
+        healthComponent.TakeDamage(damage);
+    }
+}
+public class ElementalDamageDealer : DamageDealer
+{ 
+    [SerializeField]
+    PlayerForm  playerForm;
+    protected override void DealDamage(HealthComponent healthComponent, int damage)
+    {
+        healthComponent.TakeDamage((int)damage, playerForm);
+    }
+}

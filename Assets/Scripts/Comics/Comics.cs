47f628ad393d4e4343ba988a0faae4fae149e1a8
Barsik <orlov.serg2000@gmail.com>
2024-11-05 01:41:52 +0300

    prologue and epilogue anims

--- Assets/Scripts/Comics/Comics.cs
+++ Assets/Scripts/Comics/Comics.cs
@@ -7,17 +7,25 @@ public class Comics : MonoBehaviour
     [SerializeField] private Image[] comicImages; // Массив UI Image для отображения комиксов
     [SerializeField] private int nextSceneIndex;
     private int currentIndex = 0;
+    private Animator anim;
+    private int pageIndex = 0;
 
     private void Start()
     {
         UpdateImage();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            NextImage();
+            //NextImage();
+            pageIndex++;
+            //anim.SetInteger("counter", pageIndex);
+            anim.SetTrigger("triggerpage"); 
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+            if(stateInfo.IsName("Page3")) LoadNextScene();
         }
     }
 
